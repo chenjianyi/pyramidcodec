af629ca6051d75ebdb38d997ad24729a02afec07
chenjianyi <38803228+chenjianyi@users.noreply.github.com>
2025-04-16 00:22:19 +0800

    Update sample.py

--- nanoGPT/sample.py
+++ nanoGPT/sample.py
@@ -23,7 +23,7 @@ num_samples = 1 #10 # number of samples to draw
 max_new_tokens = 1000 #500 # number of tokens generated in each sample
 temperature = 1.3 #1.0 #0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 100 #20 #200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+#seed = 1337
 device = 'cuda' #'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
